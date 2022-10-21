4d1d9ad621aebc52189d364ab5cb22622305c939
Sasha <64170613+SashaBir@users.noreply.github.com>
2022-10-21 23:13:15 +0300

    add: UniRx

--- Assets/Project/Scripts/Environment/Bonfire.cs
+++ Assets/Project/Scripts/Environment/Bonfire.cs
@@ -1,9 +1,33 @@
+using System;
+using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
 
 namespace Invaders.Environment
 {
     public class Bonfire : MonoBehaviour
     {
+        [SerializeField] private Collider _warming;
+        [SerializeField] private Collider _damaging;
+
+        private CompositeDisposable _disposable;
         
+        private void OnEnable()
+        {
+            _disposable = new CompositeDisposable();
+            
+            _warming
+                .OnCollisionEnterAsObservable()
+                .Subscribe(collision =>
+                {
+                    
+                })
+                .AddTo(_disposable);
+        }
+
+        private void OnDisable()
+        {
+            _disposable?.Dispose();
+        }
     }
 }
