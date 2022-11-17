15d3a51d22e2688e5925aff506036f6dec5f54f4
Sasha <64170613+SashaBir@users.noreply.github.com>
2022-11-17 14:40:45 +0300

    add: AK47

---
+++ Assets/Project/Scripts/Battle/Weapon/Ak47.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Invaders.Battle
+{
+    public class Ak47 : WeaponRapidFire
+    {
+        protected override IMissile Spawn(Missile missile, Transform muzzle) =>
+           Instantiate(missile, muzzle.transform.position, muzzle.rotation);
+
+        protected override void LaunchMissile(IMissile missile, Vector3 direction, float speed) =>
+            missile.Rigidbody.velocity = direction * speed;
+    }
+}
