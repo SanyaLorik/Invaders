e8ac228dfa7fb1fc62e01b995cff8d11bfa88d42
Sasha <64170613+SashaBir@users.noreply.github.com>
2022-11-18 20:47:01 +0300

    add : Explosion

---
+++ Assets/Project/Scripts/Battle/Detonation/Explosion.cs
@@ -0,0 +1,43 @@
+using Invaders.Pysiol;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Invaders.Battle
+{
+    [RequireComponent(typeof(Collider))]
+    public class Explosion : MonoBehaviour, IDamageable<int>
+    {
+        [SerializeField][Min(0)] private int _health;
+        [SerializeField][Min(0)] private int _damage;
+        [SerializeField][Min(0)] private float _radius;
+
+        public void Damage(int damage)
+        {
+            _health -= damage;
+            if (_health > 0)
+                return;
+
+            Explode();
+            Destroy(gameObject);
+        }
+
+        private IEnumerable<IDamageable<int>> Damageables
+        {
+            get
+            {
+                var objects = Physics.OverlapSphere(transform.position, _radius);
+                foreach (var o in objects)
+                {
+                    if (o.TryGetComponent(out IDamageable<int> damage) == true && damage != this)
+                        yield return damage;
+                }
+            }
+        }
+
+        private void Explode()
+        {
+            foreach (var damageable in Damageables)
+                damageable.Damage(_damage);
+        }
+    }
+}
