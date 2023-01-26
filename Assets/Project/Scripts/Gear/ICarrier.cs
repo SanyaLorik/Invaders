c2fb6b6844b4572639c3da2984b4c3f5a18fc368
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2023-01-26 20:18:23 +0300

    clear: bin code Carrier

--- Assets/Project/Scripts/Gear/ICarrier.cs
+++ Assets/Project/Scripts/Gear/ICarrier.cs
@@ -3,17 +3,6 @@ using UnityEngine;
 
 namespace Invaders.Gear
 {
-    public interface ICarrier
-    {
-        object Take();
-
-        public bool IsNearbyPortable { get; }
-
-        public bool HasPortable { get; }
-
-        public void Drop(Vector3 direction);
-    }
-
     public interface ICarrier<T>
         where T : IPortable
     {
