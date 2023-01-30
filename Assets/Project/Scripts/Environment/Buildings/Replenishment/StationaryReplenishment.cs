c096f448256e11c24a01d9894d4a29b04b593881
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2023-01-30 19:23:29 +0300

    update: replenishment system

---
+++ Assets/Project/Scripts/Environment/Buildings/Replenishment/StationaryReplenishment.cs
@@ -0,0 +1,66 @@
+﻿using Cysharp.Threading.Tasks;
+using Invaders.Battle;
+using System.Threading;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+namespace Invaders.Environment.Buildings
+{
+    public abstract class StationaryReplenishment<T> : MonoBehaviour
+    {
+        [SerializeField] private Collider _detector;
+        [SerializeField][Min(0)] private float _delay;
+
+        private CancellationTokenSource _tokenSource;
+        private CompositeDisposable _disposable;
+
+        private void OnEnable()
+        {
+            _disposable = new CompositeDisposable();
+
+            _detector
+                .OnTriggerEnterAsObservable()
+                .Subscribe(collision =>
+                {
+                    if (collision.TryGetComponent(out T replenishment) == false)
+                        return;
+
+                    _tokenSource = new CancellationTokenSource();
+                    Replenish(replenishment, _delay, _tokenSource.Token).Forget();
+                })
+                .AddTo(_disposable);
+
+            _detector
+                .OnTriggerExitAsObservable()
+                .Subscribe(collision =>
+                {
+                    if (collision.TryGetComponent(out IAmmoReplenishable replenishment) == false)
+                        return;
+
+                    _tokenSource.Cancel();
+                })
+                .AddTo(_disposable);
+        }
+
+        private void OnDisable()
+        {
+            _disposable?.Dispose();
+            _tokenSource?.Dispose();
+        }
+
+        protected async UniTaskVoid Replenish(T replenishable, float delay, CancellationToken token)
+        {
+            int millisecond = (int)(delay * 1000);
+
+            do
+            {
+                await UniTask.Delay(millisecond);
+                ChangeValue(replenishable);
+            }
+            while (token.IsCancellationRequested == false);
+        }
+
+        protected abstract void ChangeValue(T replenishable);
+    }
+}
