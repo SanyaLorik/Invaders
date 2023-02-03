a0e765134445bb30ca46a260c43aa1b062c53805
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2023-02-03 12:35:21 +0300

    add: quest framework

--- Assets/Project/Scripts/Task/Mission.cs
+++ Assets/Project/Scripts/Task/Mission.cs
@@ -5,8 +5,14 @@ namespace Invaders.Task
 {
     public abstract class Mission : MonoBehaviour
     {
+        public event Action OnDone = delegate { };
+
         [field: SerializeField] public bool IsFinally { get; protected set; }
 
         [field: SerializeField] public bool IsDone { get; protected set; }
+
+        [field: SerializeField] public bool IsActivated { get; protected set; }
+
+        public abstract void Active();
     }
 }
--- Assets/Project/Scripts/Task/Quest.cs
+++ Assets/Project/Scripts/Task/Quest.cs
@@ -7,5 +7,41 @@ namespace Invaders.Task
         [SerializeField] private Mission[] _missions;
 
         private bool _isComplated = false;
+
+        private void OnEnable()
+        {
+            foreach (var mission in _missions)
+                mission.OnDone += OnFindNextMission;
+        }
+
+        private void OnDisable()
+        {
+            foreach (var mission in _missions)
+                mission.OnDone -= OnFindNextMission;
+        }
+
+        private void OnFindNextMission()
+        {
+            if (_isComplated == true)
+                return;
+
+            foreach(var mission in _missions)
+            {
+                if (mission.IsFinally == true)
+                {
+                    _isComplated = true;
+                    break;
+                }
+
+                if (mission.IsDone == true)
+                    continue;
+
+                if (mission.IsActivated == true)
+                    break;
+
+                mission.Active();
+                break;
+            }
+        }
     }
 }
