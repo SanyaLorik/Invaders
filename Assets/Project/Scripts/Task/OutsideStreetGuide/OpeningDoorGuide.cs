507be59960b61022b638c008d00c14151d712926
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2023-02-03 13:18:35 +0300

    add: OpeningDoorGuide

---
+++ Assets/Project/Scripts/Task/OutsideStreetGuide/OpeningDoorGuide.cs
@@ -0,0 +1,32 @@
+﻿using Cysharp.Threading.Tasks;
+using System.Threading;
+using UnityEngine;
+
+namespace Invaders.Task
+{
+    public class OpeningDoorGuide : Mission
+    {
+        [SerializeField] private Transform _door;
+        [SerializeField] private float _angeY;
+
+        private CancellationTokenSource _tokenSource;
+
+        private void OnEnable()
+        {
+            _tokenSource = new CancellationTokenSource();
+            CancellationToken token = _tokenSource.Token;
+
+            UniTask.Create(async () =>
+            {
+                await UniTask.WaitWhile(() => _door.eulerAngles.y <= _angeY, cancellationToken: token);
+                base.Complate();
+            });
+        }
+
+        private void OnDisable()
+        {
+            _tokenSource?.Cancel();
+            _tokenSource?.Dispose();
+        }
+    }
+}
