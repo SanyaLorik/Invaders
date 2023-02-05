e68d9056ffc1cecc9692b8a92c645857c5f7b9ee
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2023-02-05 16:12:59 +0300

    update: ui health

--- Assets/Project/Scripts/Ui/Player/UiPlayerHealth.cs
+++ Assets/Project/Scripts/Ui/Player/UiPlayerHealth.cs
@@ -1,6 +1,8 @@
-﻿using Invaders.Additionals;
+﻿using DG.Tweening;
+using Invaders.Additionals;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using Zenject;
 
 namespace Invaders.Ui
@@ -8,6 +10,9 @@ namespace Invaders.Ui
     public class UiPlayerHealth : MonoBehaviour
     {
         [SerializeField] private TMP_Text _health;
+        [SerializeField] private Image _progresColorBar;
+        [SerializeField] private Color _initial;
+        [SerializeField] private Color _final;
 
         private IValueObserver<int, int> _healthObserver;
 
@@ -21,7 +26,18 @@ namespace Invaders.Ui
         private void OnDisable() =>
             _healthObserver.OnChanged -= OnShowChangeHealth;
 
-        private void OnShowChangeHealth(int remaining, int total) =>
-            _health.text = $"HP {remaining}/{total}";
+        private void OnShowChangeHealth(int remaining, int total)
+        {
+            _health.text = remaining.ToString();
+            ChangeColor(remaining, total);
+        }
+
+        private void ChangeColor(int remaining, int total)
+        {
+            Color color = Color.Lerp(_initial, _final, 1 / remaining / total);
+            const float duration = 1f;
+
+            _progresColorBar.DOColor(color, duration);
+        }
     }
 }
