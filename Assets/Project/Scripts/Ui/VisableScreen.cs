b7bf8ab7c04f257da5bcf96a4fc25d7ab8a37060
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2023-02-05 23:45:30 +0300

    revision: scene

--- Assets/Project/Scripts/Ui/VisableScreen.cs
+++ Assets/Project/Scripts/Ui/VisableScreen.cs
@@ -12,7 +12,7 @@ namespace Invaders.Ui
         [SerializeField][Range(0f, 3f)] private float _duration;
 
         public void Ascend() =>
-            _screen.DOColor(_ascended, _duration).OnComplete(() => _screen.gameObject.SetActive(true));
+            _screen.DOColor(_ascended, _duration).OnComplete(() => _screen.gameObject.SetActive(false));
 
         public void Fade() =>
             _screen.DOColor(_faded, _duration).OnComplete(() => _screen.gameObject.SetActive(true));
