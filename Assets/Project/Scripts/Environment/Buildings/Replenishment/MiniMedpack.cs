0c851f0a0a2f25b7fcf4d3b2b698cf48acce3144
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2023-02-06 22:15:34 +0300

    add: mini medpack

---
+++ Assets/Project/Scripts/Environment/Buildings/Replenishment/MiniMedpack.cs
@@ -0,0 +1,13 @@
+﻿using Invaders.Entities;
+using UnityEngine;
+
+namespace Invaders.Environment.Buildings
+{
+    public class MiniMedpack : SingleStationaryReplenishment<IPlayer>
+    {
+        [SerializeField][Range(0, 20)] private int _addingHealth;
+
+        protected override void ChangeValue(IPlayer replenishable) =>
+            replenishable.Value.Add(_addingHealth);
+    }
+}
--- Assets/Project/Scripts/Movement/Modes/VelocityMovement.cs
+++ Assets/Project/Scripts/Movement/Modes/VelocityMovement.cs
@@ -14,7 +14,15 @@ namespace Invaders.Movement
             _speed = speed;
         }
 
-        public void Move(Vector3 direction) =>
-            _rigidbody.velocity = direction * _speed.Current;
+        public void Move(Vector3 direction)
+        {
+            Vector3 velocity = direction * _speed.Current;
+            _rigidbody.velocity = new Vector3()
+            {
+                x = velocity.x,
+                y = _rigidbody.velocity.y,
+                z = velocity.z
+            };
+        }
     }
 }
