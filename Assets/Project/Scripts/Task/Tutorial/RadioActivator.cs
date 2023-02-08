10bc9ea7f84c44fa04fa1df05e94a6bc45063308
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2023-02-08 14:25:36 +0300

    fix: RadioActivator guide

--- Assets/Project/Scripts/Task/Tutorial/RadioActivator.cs
+++ Assets/Project/Scripts/Task/Tutorial/RadioActivator.cs
@@ -8,6 +8,7 @@ namespace Invaders.Task
     public class RadioActivator : Mission
     {
         [SerializeField] private Radio _radio;
+        [SerializeField] private GameObject _wholeRadio;
 
         private CancellationTokenSource _tokenSource;
 
@@ -16,11 +17,18 @@ namespace Invaders.Task
             _tokenSource = new CancellationTokenSource();
             CancellationToken token = _tokenSource.Token;
 
-            UniTask.Create(async () =>
+            UniTask player = UniTask.Create(async () =>
             {
                 await UniTask.WaitWhile(() => _radio.IsPlayed == false, cancellationToken: token);
                 await UniTask.Delay(2000, cancellationToken: token); // 2000 - delay for listing music
+            });
 
+            UniTask destroyed = UniTask.Create(async () =>
+                await UniTask.WaitWhile(() => _wholeRadio.activeSelf == true, cancellationToken: token));
+
+            UniTask.Create(async () =>
+            {
+                await UniTask.WhenAny(player, destroyed);
                 base.Complate();
             });
         }
