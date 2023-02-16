c5e542ae359d6f6efd843fcc368a881e69f186f8
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2023-02-16 12:38:29 +0300

    add: tag

--- Assets/Project/Scripts/Movement/SurfaceSlider.cs
+++ Assets/Project/Scripts/Movement/SurfaceSlider.cs
@@ -6,10 +6,15 @@ namespace Invaders.Movement
     [RequireComponent(typeof(Rigidbody))]
     public class SurfaceSlider : MonoBehaviour
     {
+        [SerializeField][Min(0)] private int _groundLayerId;
+
         private Vector3 _normal;
 
-        private void OnCollisionEnter(Collision collision) =>
-            _normal = collision.contacts[0].normal;
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.layer == _groundLayerId)
+                _normal = collision.contacts[0].normal;
+        }
 
         public Vector3 Project(Vector3 forward) =>
             forward - Vector3.Dot(forward, _normal) * _normal;
