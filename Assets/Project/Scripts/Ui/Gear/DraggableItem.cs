9325e1a12f3a9e8abea643b3e4f2a52718743589
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2023-02-18 17:38:58 +0300

    update: draggable item

--- Assets/Project/Scripts/Ui/Gear/DraggableItem.cs
+++ Assets/Project/Scripts/Ui/Gear/DraggableItem.cs
@@ -1,35 +1,60 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
 namespace Invaders.Ui
 {
-    [RequireComponent(typeof(RectTransform))]
-    [RequireComponent(typeof(Image))]
     public class DraggableItem : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
     {
-        public Canvas canvas;
+        [Header("Size")]
+        [SerializeField] private Canvas _canvas;
 
-        private RectTransform _rect;
+        [Header("Items")]
+        [SerializeField] private Transform _draggableArea;
 
-        private void Awake()
-        {
-            _rect = GetComponent<RectTransform>();
-        }
+        private RectTransform _item;
+
+        private Transform _returned;
 
         public void OnBeginDrag(PointerEventData eventData)
         {
+            if (eventData.pointerEnter == null)
+                return;
 
+            if (eventData.pointerEnter.TryGetComponent(out InventorySlot inventorySlot) == false)
+                return;
+
+            _returned = inventorySlot.transform;
+            _item = inventorySlot.Item;
+            _item.SetParent(_draggableArea);
         }
 
         public void OnDrag(PointerEventData eventData)
         {
-            _rect.anchoredPosition += eventData.delta / canvas.scaleFactor;
+            if (_item != null)
+                _item.anchoredPosition += eventData.delta / _canvas.scaleFactor;
         }
 
         public void OnEndDrag(PointerEventData eventData)
         {
+            if (_item == null)
+                return;
 
+            if (eventData.pointerEnter == null)
+                return;
+
+            if (eventData.pointerEnter.TryGetComponent(out InventorySlot inventorySlot) == false)
+            {
+                _item.SetParent(_returned);
+                _item.localPosition = Vector3.zero;
+                return;
+            }
+
+            _item.SetParent(inventorySlot.transform);
+            _item.transform.localPosition = Vector3.zero;
+
+            inventorySlot.Item.SetParent(_returned);
+            inventorySlot.Item.localPosition = Vector3.zero;
+            _item = null;
         }
     }
 }
---
+++ Assets/Project/Scripts/Ui/Gear/InventorySlot.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Invaders.Ui
+{
+    public class InventorySlot : MonoBehaviour
+    {
+        [field: SerializeField] public RectTransform Item { get; set; }
+    }
+}
