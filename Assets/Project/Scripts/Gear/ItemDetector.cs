c5a8949184702ebfc390a3ef8a67349fc83cc483
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2023-02-28 17:27:47 +0300

    update: item detector

---
+++ Assets/Project/Scripts/Gear/ItemDetector.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Invaders.Gear
+{
+    [RequireComponent(typeof(Collider))]
+    public class ItemDetector : MonoBehaviour
+    {
+        private readonly IList<IItem> _items = new List<IItem>();
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.TryGetComponent(out IItem item) == true)
+                _items.Add(item);
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.TryGetComponent(out IItem item) == true)
+                _items.Remove(item);
+        }
+
+        public bool HaveItemInArea => _items.Count > 0;
+
+        public IItem Receive()
+        {
+            IItem item = _items[0];
+            _items.Remove(item);
+
+            return item;
+        }
+    }
+}
