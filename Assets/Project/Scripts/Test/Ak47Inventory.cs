ad31f9577197f8293acf435b15194b5a4a0c4778
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2023-03-05 21:24:49 +0300

    fix: fields inheritance

--- Assets/Project/Scripts/Test/Ak47Inventory.cs
+++ Assets/Project/Scripts/Test/Ak47Inventory.cs
@@ -1,22 +1,9 @@
 ﻿using Invaders.Battle;
-using UnityEngine;
 
 namespace Invaders.Test
 {
     public class Ak47Inventory : Ak47
     {
-        [field: SerializeField] public Sprite Icon {get; set;}
 
-        public uint Count => 1;
-
-        public void Show()
-        {
-            gameObject.SetActive(true);
-        }
-
-        public void Hide()
-        {
-            gameObject.SetActive(false);
-        }
     }
 }
