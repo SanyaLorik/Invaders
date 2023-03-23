8e833354b777d2d3fcf43f9c1bf1b1c5b05cd8f2
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2023-03-24 00:59:46 +0300

    fix: quests

--- Assets/Project/Scripts/Task/Tutorial/TakenWeaponGuide.cs
+++ Assets/Project/Scripts/Task/Tutorial/TakenWeaponGuide.cs
@@ -1,7 +1,20 @@
+using Invaders.Gear;
+using Invaders.Ui;
+using UnityEngine;
+
 namespace Invaders.Task
 {
     public class TakenWeaponGuide : Mission
     {
+        [SerializeField] private SpeciallyInventorySlot<IItem> _used;
 
+        private void OnEnable() =>
+            _used.OnTaken += OnTake;
+
+        private void OnDisable() =>
+            _used.OnTaken -= OnTake;
+
+        private void OnTake(IItem item) =>
+            base.Complate();
     }
 }
